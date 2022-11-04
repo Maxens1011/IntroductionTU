1bff4beee616d2f10815f97a7b7f5873c61635ff
Maxens Thiam <MaxensThiam@outlook.com>
2022-11-04 16:48:24 +0100

    did the exercise

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -7,6 +7,19 @@ using UnityEngine.Events;
 
 public struct Health
 {
-    
-
+    private int _maxHealth;
+    private int _health;
+    public int MaxHealth { get { return _maxHealth; } set { _maxHealth = value; } }
+    public int CurrentHealth { get { return _health; } set { _health = value; } }
+    public bool IsDead { get { return (_health <= 0); } }
+    public Health(int maxHealth, int health)
+    {
+        _maxHealth = maxHealth;
+        _health = health;
+    }
+    public Health(int health)
+    {
+        _health = health;
+        _maxHealth = health;
+    }
 }
--- Assets/MyVector3.cs
+++ Assets/MyVector3.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public struct MyVector3
 {
-    
+    float x, y, z;
+    public float X { get => x; set => x = value; }
+    public float Y { get => y; set => y = value; }
+    public float Z { get => z; set => z = value; }
+
+    public float SqrtMagnitude => x * x + y * y + z * z;
+    public float Magnitude => Mathf.Sqrt(SqrtMagnitude);
+
+
+
+    public void Normalize()
+    {
+        var m = Magnitude;
+        x /= m;
+        y /= m;
+        z /= m;
+    }
+
+    public static MyVector3 operator +(MyVector3 left, MyVector3 right)
+    {
+        return new MyVector3(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+    }
+    public static MyVector3 operator *(MyVector3 left, float n)
+    {
+        return new MyVector3(left.X * n, left.Y * n, left.Z * n);
+    }
+    public MyVector3(float X, float Y, float Z)
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
 }
